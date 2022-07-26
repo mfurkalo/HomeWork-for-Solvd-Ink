b5b079c894a170ce9c4949950f8d805c49b5c86c
Maksym <maximkack@gmail.com>
2022-07-26 18:48:07 -0400

    added sorting order

--- QuickSort.java
+++ QuickSort.java
@@ -11,25 +11,30 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-        System.out.println("Let's sort this array: " + Arrays.toString(array));
-        quickSort(array, 0, array.length - 1);
+        System.out.println("Let's sort ascending this array: " + Arrays.toString(array));
+        quickSort(array, 0, array.length - 1, false);
+        System.out.println("The array is sorted now: " + Arrays.toString(array));
+        System.out.println("Let's sort descending  the array");
+        quickSort(array, 0, array.length - 1, true);
         System.out.println("The array is sorted now: " + Arrays.toString(array));
     }
 
     //a simple swap function
     static void swap(int[] array, int low, int pivot) {
 
-        array[low] = array[low] + array[pivot];
-        array[pivot] = array[low] - array[pivot];
-        array[low] = array[low] - array[pivot];
+        int tmp = array[low];
+        array[low] = array[pivot];
+        array[pivot] = tmp;
     }
 
     // finding the partition element's index
-    static int partition(int[] array, int low, int high) {
+    static int partition(int[] array, int low, int high, boolean descending) {
         int p = low, i;
 
         for (i = low + 1; i <= high; i++)
-            if (array[i] < array[low])
+            if ((!descending) && (array[i] < array[low]))
+                swap(array, ++p, i);
+            else if ((descending) && (array[i] > array[low]))
                 swap(array, ++p, i);
 
         swap(array, low, p);
@@ -37,12 +42,12 @@ public class QuickSort {
     }
 
     //It splits the array and  works recursively
-    static void quickSort(int[] array, int low, int high) {
+    static void quickSort(int[] array, int low, int high, boolean descending) {
 
         if (low < high) {
-            int p = partition(array, low, high);
-            quickSort(array, low, p - 1);
-            quickSort(array, p + 1, high);
+            int p = partition(array, low, high, descending);
+            quickSort(array, low, p - 1, descending);
+            quickSort(array, p + 1, high, descending);
         }
     }
 }
